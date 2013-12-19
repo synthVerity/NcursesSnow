cb74528f0c606aaedfa24e8439ba686b4182b616
Austin Karn <karna@sunyit.edu>
2013-12-18 22:29:09 -0500

    Small game created with previous program
    Created small game using previous program

---
+++ snowGame.c
@@ -0,0 +1,119 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <ncurses.h>
+
+#define SNOWFLAKES 50
+
+// Struct that holds placement information for each snowflake
+struct snow
+{
+	int x;
+	int y;
+};
+
+// Obvious main is obvious
+int main(void)
+{
+	// All the variables used, including the array of snowflakes
+	int i;
+	int running = 1;
+	int maxY, maxX;
+	int playX, playY;
+	char piece = '*';
+	char player = '#';
+	struct snow snowflakes[SNOWFLAKES];
+
+	// Seed random
+	srand(time(NULL));
+
+	// Start the ncurses library, stdscr
+	initscr();
+
+	// Ncurses values used to set various settings
+	keypad(stdscr, TRUE);
+	halfdelay(1);
+	curs_set(0);
+
+	// Get size of terminal used for program
+	getmaxyx(stdscr, maxY, maxX);
+
+	playY = maxY - 1;
+	playX = maxX / 2;
+
+	// Initialize the snowflake locations
+	for(i = 0; i < SNOWFLAKES; i++)
+	{
+		snowflakes[i].x = rand() % maxX;
+		snowflakes[i].y = rand() % maxY;
+	}
+
+	// Main animation loop
+	while(running)
+	{
+		clear();
+
+		// Output of the clouds
+		for(i = 0; i < maxX; i++)
+		{
+			mvprintw(0, i, "&");
+		}
+
+		// Output of the individual snowflakes
+		for(i = 0; i < SNOWFLAKES; i++)
+		{
+			mvprintw(snowflakes[i].y, snowflakes[i].x, "%c", piece);
+
+			// Sets another on the top when snowflakes reach the bottom of the screen
+			if(snowflakes[i].y > maxY)
+			{
+				snowflakes[i].x = rand() % maxX;
+				snowflakes[i].y = 0;
+			}
+
+			// To simulate a snowflake falling
+			snowflakes[i].y++;
+		}
+
+		// Output of player piece
+		mvprintw(playY, playX, "%c", player);
+
+		// User input to cancel the program and move the player piece
+		switch(getch())
+		{
+			case KEY_F(1):
+				running = 0;
+				break;
+
+			case KEY_LEFT:
+				if(playX <= 0)
+					break;
+
+				playX--;
+
+				break;
+
+			case KEY_RIGHT:
+				if(playX >= maxX - 1)
+					break;
+
+				playX++;
+
+				break;
+
+		}
+
+		for(i = 0; i < SNOWFLAKES; i++)
+		{
+			if(playX == snowflakes[i].x && playY == snowflakes[i].y - 1)
+			{
+				running = 0;
+			}
+		}
+
+	}
+
+	// Ends ncurses and the initial program
+	endwin();
+	return 0;
+}
